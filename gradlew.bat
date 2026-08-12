c40167c8debc27a4ce57c59a6bf2ff1efa85d520
Beilin-Y <3955064047@qq.com>
2026-08-12 11:19:38 +0800

    Update gradlew.bat for improved Java execution

--- gradlew.bat
+++ gradlew.bat
@@ -1,9 +1,19 @@
 @echo off
-setlocal
+:: -----------------------------------------------------------------------------
+:: Gradle start up script for Windows
+:: -----------------------------------------------------------------------------
+
+SETLOCAL
+
 set DIRNAME=%~dp0
-set APP_HOME=%DIRNAME%..\
-set JAVA_EXE=%JAVA_HOME%\bin\java.exe
-if exist %JAVA_EXE% goto haveJavaHome
-set JAVA_EXE=java
-:haveJavaHome
-"%JAVA_EXE%" -jar "%APP_HOME%gradle\wrapper\gradle-wrapper.jar" %*
+set APP_BASE_DIR=%DIRNAME%
+set GRADLE_HOME=%APP_BASE_DIR%gradle\wrapper
+set CLASSPATH=%GRADLE_HOME%\gradle-wrapper.jar
+
+if defined JAVA_HOME (
+  set RUNJAVA=%JAVA_HOME%\bin\java
+) else (
+  set RUNJAVA=java
+)
+
+"%RUNJAVA%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
